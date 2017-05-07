ea1dc55778fec53b4c064a284fc0f4169e167761
justbobi <justbobi@Bobi-PC>
2017-05-07 18:05:58 +0300

    Design Patterns

---
+++ Task 8/src/com/omisoft/basic_java/DesignPatterns/task2/Singleton.java
@@ -0,0 +1,32 @@
+package com.omisoft.basic_java.DesignPatterns.task2;
+/**
+ * Creating a Singleton class
+ * @author bkoprinski
+ *
+ */
+public class Singleton {
+	private static Singleton instance = new Singleton();
+	
+	/**
+	 * Constructor
+	 */
+	private Singleton()
+	{
+		System.out.println("Singleton created");
+	}
+
+	/**
+	 * @return the instance
+	 */
+	public static Singleton getInstance() {
+		return instance;
+	}
+
+	/**
+	 * @param instance the instance to set
+	 */
+	public static void setInstance(Singleton instance) {
+		Singleton.instance = instance;
+	}
+	
+}
